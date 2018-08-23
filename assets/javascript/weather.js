e2631655aabcc0d1b67fd89edbe4b30c03173f7a
unknown <dkmeade@gmail.com>
2018-08-22 21:20:48 -0600

    added secure link to url for openweathermap

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -3,7 +3,7 @@ navigator.geolocation.getCurrentPosition(function(position) {
     console.log(position);
     var {latitude, longitude}=position.coords
     console.log(latitude, longitude);
-    var url2=`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&&lon=${longitude}&APPID=84c07ea9b4ea804915b8683ab42742b4&units=imperial`
+    var url2=`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&&lon=${longitude}&APPID=84c07ea9b4ea804915b8683ab42742b4&units=imperial`
     fetch(url2)
     .then(function(response) {
         return response.json();
